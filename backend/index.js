f1b6228a1bab59a20be14cebbf6cb223fb89546a
ankitmgs <85329746+ankitmgs@users.noreply.github.com>
2023-01-19 19:49:00 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const userRouter = require("./routers/userRouter");
+const cors = require("cors");
+
+const port = 5000;
+
+// to allow your frontend
+app.use(cors({ origin: ["http://localhost:3000"] }));
+app.use(express.json());
+
+
+//middleware
+app.use("/user", userRouter);
+
+
+//to start server
+app.listen(port, () => {
+  console.log("server started");
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      dljnekfjn
+    </div>
+  );
+}
+
+export default App;
