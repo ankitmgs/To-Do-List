ea680826701207e685fcdadf6ccdc26430b85dd7
ankitmgs <85329746+ankitmgs@users.noreply.github.com>
2023-01-24 12:16:32 +0530

    ...

--- backend/modals/todoModals.js
+++ backend/modals/todoModals.js
@@ -1,12 +1,13 @@
 // import mongoose to create new Schema
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 //create schema
 const ToDoItemSchema = new mongoose.Schema({
-    item:{
-        type:String,
-        required:true
-    }
-})
+  item: {
+    type: String,
+    required: true,
+  },
+  created: { type: Date, default: new Date() },
+});
 // export this schema
-module.exports = mongoose.model('todo' ,ToDoItemSchema);
+module.exports = mongoose.model("todo", ToDoItemSchema);
